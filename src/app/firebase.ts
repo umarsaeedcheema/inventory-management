1d5b5ae9354185c344da4fa8949d652217509a05
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-01 23:36:54 +0300

    update readme

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBCabKduIWIvXM21xDfJ5cdReBxlYZ7AFs",
