a5b9b04f95b0c97f738ca1f16d780e807dbcc075
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-02 21:28:35 +0300

    add logout button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
 "use client";
 import { useAuth } from "../app/components/login/authContext";
 import { useRouter } from "next/navigation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Table from "./components/table/table";
+import { Button } from "@mui/material";
 
 export default function Home() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
   const router = useRouter();
+  const [loggedIn, setLoggedIn] = useState(false);
 
   useEffect(() => {
-    if (!user) {
+    if (user) {
+      setLoggedIn(true);
+    } else {
       router.push("/components/login");
     }
   }, [user, router]);
@@ -18,8 +22,24 @@ export default function Home() {
     return null;
   }
 
+  const handleSignOut = async () => {
+    await signOut();
+    setLoggedIn(false);
+    router.push("/components/login");
+  };
+
   return (
     <main>
+      {loggedIn && (
+        <Button
+          sx={{ position: "absolute", left: "90%" }}
+          variant="contained"
+          color="primary"
+          onClick={handleSignOut}
+        >
+          Logout
+        </Button>
+      )}
       <Table />
     </main>
   );
