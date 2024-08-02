b49b3a517bdfa3a9470316be28f6419bfabc5538
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-02 21:18:11 +0300

    fix routing if user is not logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,23 @@
+"use client";
+import { useAuth } from "../app/components/login/authContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import Table from "./components/table/table";
 
-export default function Page() {
+export default function Home() {
+  const { user } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/components/login");
+    }
+  }, [user, router]);
+
+  if (!user) {
+    return null;
+  }
+
   return (
     <main>
       <Table />
