c322f98e81b6a81252b2b5401f6a3f8c944dda22
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-03 13:52:21 +0300

    update navbar component
    using user from authContext to to manage state

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,14 +1,45 @@
-import React from "react";
-import { AppBar, Toolbar, Typography, Button } from "@mui/material";
+"use client";
 
-const Navbar = () => {
+import React from "react";
+import { AppBar, Toolbar, Typography, Button, Box } from "@mui/material";
+import { useAuth } from "./login/authContext";
+import { useRouter } from "next/navigation";
+
+const Navbar: React.FC = () => {
+  const { user, signInWithGoogle, signOut } = useAuth();
+  console.log(user);
+  const router = useRouter();
+
+  const handleSignin = async () => {
+    await signInWithGoogle();
+    router.push("/components/inventory");
+  };
+
+  const handleSignOut = async () => {
+    await signOut();
+    router.push("/");
+  };
+
   return (
     <AppBar position="static" sx={{ marginBottom: "20px" }}>
       <Toolbar>
         <Typography variant="h6" sx={{ flexGrow: 1 }}>
           Inventory Tracker
         </Typography>
-        <Button color="inherit">Login</Button>
+        {user ? (
+          <Box sx={{ display: "flex" }}>
+            <Typography variant="h6" sx={{ marginRight: "10px" }}>
+              {user.displayName}
+            </Typography>
+            <Button color="inherit" onClick={handleSignOut}>
+              Logout
+            </Button>
+          </Box>
+        ) : (
+          <Button color="inherit" onClick={handleSignin}>
+            Login
+          </Button>
+        )}
       </Toolbar>
     </AppBar>
   );
