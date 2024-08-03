fbfa3028f4b1d6924d25806dd9abd68d941516fe
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-03 14:59:27 +0300

    create login page

---
+++ src/app/components/login/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { useAuth } from "./authContext";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Button, Box, Typography } from "@mui/material";
+
+const LoginPage = () => {
+  const { user, signInWithGoogle } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (user) {
+      router.push("/");
+    }
+  }, [user, router]);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Login Page
+      </Typography>
+      <Button variant="contained" onClick={signInWithGoogle}>
+        Sign in with Google
+      </Button>
+    </Box>
+  );
+};
+
+export default LoginPage;
