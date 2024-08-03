96679963395e62b1933930126ca77a4b036c48e5
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-03 13:50:41 +0300

    improve home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,102 @@
-"use client";
-import { useAuth } from "../app/components/login/authContext";
-import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
-import Table from "./components/table/table";
-import { Button } from "@mui/material";
+import Navbar from "./components/navbar";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { Button, Link, Typography } from "@mui/material";
 
 export default function Home() {
-  const { user, signOut } = useAuth();
-  const router = useRouter();
-  const [loggedIn, setLoggedIn] = useState(false);
-
-  useEffect(() => {
-    if (user) {
-      setLoggedIn(true);
-    } else {
-      router.push("/components/login");
-    }
-  }, [user, router]);
-
-  if (!user) {
-    return null;
-  }
-
-  const handleSignOut = async () => {
-    await signOut();
-    setLoggedIn(false);
-    router.push("/components/login");
-  };
-
   return (
     <main>
-      {loggedIn && (
-        <Button
-          sx={{ position: "absolute", left: "90%" }}
-          variant="contained"
-          color="primary"
-          onClick={handleSignOut}
+      <Box sx={{ bgcolor: "#cfe8fc", minHeight: "100vh" }}>
+        <Navbar />
+
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "80vh",
+          }}
         >
-          Logout
-        </Button>
-      )}
-      <Table />
+          <Paper
+            elevation={12}
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              width: "50%",
+              height: "30%",
+              marginBottom: 4,
+            }}
+          >
+            <Typography variant="h4">Welcome to Inventory Tracker</Typography>
+          </Paper>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-around",
+              width: "80%",
+            }}
+          >
+            <Paper
+              elevation={6}
+              sx={{
+                bgcolor: "#fff",
+                padding: 2,
+                borderRadius: 1,
+                boxShadow: 3,
+                flex: 1,
+                margin: 1,
+              }}
+            >
+              <Typography variant="h6" gutterBottom>
+                Feature 1
+              </Typography>
+              <Typography variant="body1">Description of Feature 1.</Typography>
+            </Paper>
+            <Paper
+              elevation={6}
+              sx={{
+                bgcolor: "#fff",
+                padding: 2,
+                borderRadius: 1,
+                boxShadow: 3,
+                flex: 1,
+                margin: 1,
+              }}
+            >
+              <Typography variant="h6" gutterBottom>
+                Feature 2
+              </Typography>
+              <Typography variant="body1">Description of Feature 2.</Typography>
+            </Paper>
+            <Paper
+              elevation={6}
+              sx={{
+                bgcolor: "#fff",
+                padding: 2,
+                borderRadius: 1,
+                boxShadow: 3,
+                flex: 1,
+                margin: 1,
+              }}
+            >
+              <Typography variant="h6" gutterBottom>
+                Feature 3
+              </Typography>
+              <Typography variant="body1">Description of Feature 3.</Typography>
+            </Paper>
+          </Box>
+          <Button
+            variant="contained"
+            LinkComponent={Link}
+            href="/components/inventory"
+          >
+            Get Started!
+          </Button>
+        </Box>
+      </Box>
     </main>
   );
 }
