db9838ec44c2fb198604acffe4d319ae840d6a87
Umar Saeed Cheema <23100206@lums.edu.pk>
2024-08-03 15:00:23 +0300

    add protect layout conditionally

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -1,16 +1,23 @@
+"use client";
 import Navbar from "./navbar";
 import { Box } from "@mui/material";
+import { usePathname } from "next/navigation";
 import ProtectedLayout from "./protected-layout";
 
 export default function Layout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
+  const pathname = usePathname();
+  const isExcludedRoute = pathname === "/components/login" || pathname === "/";
+
   return (
-    <ProtectedLayout>
-      <Box sx={{ bgcolor: "#cfe8fc" }}>
-        <Navbar />
-        {children}
-      </Box>
-    </ProtectedLayout>
+    <Box sx={{ bgcolor: "#cfe8fc", minHeight: "100vh" }}>
+      <Navbar />
+      {isExcludedRoute ? (
+        children
+      ) : (
+        <ProtectedLayout>{children}</ProtectedLayout>
+      )}
+    </Box>
   );
 }
